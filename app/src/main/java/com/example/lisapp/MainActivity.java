92a044f548aeaadec731a1637074cce0aea7be61
Your Name <lisa.balke@yahoo.se>
2024-01-29 14:42:01 +0100

    inget speciellt

--- app/src/main/java/com/example/lisapp/MainActivity.java
+++ app/src/main/java/com/example/lisapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void accelerometer(){
+    public void accelerometer(View v){
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
+
 }
